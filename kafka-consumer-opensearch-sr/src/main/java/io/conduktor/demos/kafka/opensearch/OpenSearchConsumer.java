58063ea066182c35b95e5376478d40756548ae5a
sraakesh95github <raakesh95jobs@gmail.com>
2024-03-25 17:05:06 -0400

    Consumer init

---
+++ kafka-consumer-opensearch-sr/src/main/java/io/conduktor/demos/kafka/opensearch/OpenSearchConsumer.java
@@ -0,0 +1,16 @@
+package io.conduktor.demos.kafka.opensearch;
+
+public class OpenSearchConsumer {
+    public static void main(String[] args) {
+
+        //create an opensearch client
+
+
+        //create kafka client
+
+        //main code logic
+
+        //close the connection
+
+    }
+}
